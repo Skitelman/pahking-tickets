6c55a97d6309ca1fb6dadaa5d664ff25d7a8400c
Sam Ghitelman <samg@zillowgroup.com>
2018-04-27 17:03:20 -0400

    Refactor TicketListItem

--- src/components/ticket_list_item.js
+++ src/components/ticket_list_item.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TicketDetail from './ticket_detail';
 
+class TicketListItem extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-const TicketListItem = ({ticket, selectedTicket, onTicketSelect}) => {
-  const active = selectedTicket === ticket ? 'active' : '';
+  isSelectedTicket() {
+    const selected = this.props.selectedTicket === this.props.ticket;
+    return selected;
+  }
 
-  return (
-    <div>
-      <div
-        className={`list-group-item list-group-item-action ${active} ticket-list-item`}
-        onClick={() => onTicketSelect(ticket)}>
-        {ticket.violation}
-      </div>
-      { selectedTicket === ticket &&
-        <div className={`list-group-item list-group-item-action ticket-list-item`}>
-          <TicketDetail ticket={selectedTicket} />
+  isActive() {
+    const active = this.isSelectedTicket() ? 'active' : '';
+    return active;
+  }
+
+  render () {
+    const {ticket, onTicketSelect, selectedTicket } = this.props;
+    return (
+      <div>
+        <div
+          className={`list-group-item list-group-item-action ${this.isActive()} ticket-list-item`}
+          onClick={() => onTicketSelect(ticket)}>
+          {ticket.violation}
         </div>
-      }
-    </div>
-  );
-};
+        { this.isSelectedTicket() &&
+          <div className={`list-group-item list-group-item-action ticket-list-item`}>
+            <TicketDetail ticket={selectedTicket} />
+          </div>
+        }
+      </div>
+    );
+  }
+}
 
 export default TicketListItem;
